868f5a06e8e4f1f656d92c631c8c9cc3f7e445d6
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-09-30 17:37:29 +0700

    new commit for UAS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
+import { Roboto } from '@next/font/google';
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} `}
       >
-        {children}
+          {children}
       </body>
     </html>
   );
